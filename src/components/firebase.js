dbf36eab48a9b127cc7889bc1e5d25569aff7d09
compean <javier.compeanr@gmail.com>
2021-09-01 19:12:28 -0500

    initiation project

---
+++ src/components/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB2pWQgs_XuiWCKgVoYUz81HbivVz1DV_E",
+  authDomain: "netflix-build-17a33.firebaseapp.com",
+  projectId: "netflix-build-17a33",
+  storageBucket: "netflix-build-17a33.appspot.com",
+  messagingSenderId: "89386997128",
+  appId: "1:89386997128:web:2c00b9856acdfcad93163d"
+};
